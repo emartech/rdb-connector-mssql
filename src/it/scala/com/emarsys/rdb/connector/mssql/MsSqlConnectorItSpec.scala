676cd1723f8a316252678b702eaad9bd1b81cb3b
Miklos Martin <miklos.martin@gmail.com>
2018-10-10 14:54:46 +0200

    RDS-223 improve error handling

--- src/it/scala/com/emarsys/rdb/connector/mssql/MsSqlConnectorItSpec.scala
+++ src/it/scala/com/emarsys/rdb/connector/mssql/MsSqlConnectorItSpec.scala
@@ -1,15 +1,28 @@
 package com.emarsys.rdb.connector.mssql
 
-import com.emarsys.rdb.connector.common.models.Errors.{ConnectionConfigError, ConnectionError}
+import akka.NotUsed
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{Sink, Source}
+import akka.testkit.TestKit
+import com.emarsys.rdb.connector.common.ConnectorResponse
+import com.emarsys.rdb.connector.common.models.Errors._
 import com.emarsys.rdb.connector.mssql.utils.TestHelper
-import org.scalatest.{Matchers, WordSpecLike}
+import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
 import slick.util.AsyncExecutor
 
 import scala.concurrent.Await
-import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
 
-class MsSqlConnectorItSpec extends WordSpecLike with Matchers {
+class MsSqlConnectorItSpec
+    extends TestKit(ActorSystem("connector-it-soec"))
+    with WordSpecLike
+    with Matchers
+    with BeforeAndAfterAll {
+
+  implicit val mat      = ActorMaterializer()
+  override def afterAll = TestKit.shutdownActorSystem(system)
 
   "MySqlConnectorItSpec" when {
 
@@ -83,5 +96,59 @@ class MsSqlConnectorItSpec extends WordSpecLike with Matchers {
 
     }
 
+    trait QueryRunnerScope {
+      lazy val connectionConfig = testConnection
+      lazy val queryTimeout     = 1.second
+
+      def runQuery(q: String): ConnectorResponse[Unit] =
+        for {
+          Right(connector) <- MsSqlConnector(connectionConfig)(AsyncExecutor.default())
+          Right(source)    <- connector.rawSelect(q, limit = None, queryTimeout)
+          res              <- sinkOrLeft(source)
+          _ = connector.close()
+        } yield res
+
+      def sinkOrLeft[T](source: Source[T, NotUsed]): ConnectorResponse[Unit] =
+        source
+          .runWith(Sink.ignore)
+          .map[Either[ConnectorError, Unit]](_ => Right(()))
+          .recover {
+            case e: ConnectorError => Left[ConnectorError, Unit](e)
+          }
+    }
+
+    "Custom error handling" should {
+      "recognize query timeouts" in new QueryRunnerScope {
+        val result = Await.result(runQuery("waitfor delay '00:00:02.000'; select 1"), 2.second)
+
+        result shouldBe a[Left[_, _]]
+        result.left.get shouldBe an[QueryTimeout]
+      }
+
+      "recognize syntax errors" in new QueryRunnerScope {
+        val result = Await.result(runQuery("select from table"), 1.second)
+
+        result shouldBe a[Left[_, _]]
+        result.left.get shouldBe an[SqlSyntaxError]
+      }
+
+      "recognize access denied errors" in new QueryRunnerScope {
+        override lazy val connectionConfig = TestHelper.TEST_CONNECTION_CONFIG.copy(
+          dbUser = "unprivileged",
+          dbPassword = "unprivileged1!"
+        )
+        val result = Await.result(runQuery("select * from access_denied"), 1.second)
+
+        result shouldBe a[Left[_, _]]
+        result.left.get shouldBe an[AccessDeniedError]
+      }
+
+      "recognize if a table is not found" in new QueryRunnerScope {
+        val result = Await.result(runQuery("select * from a_non_existing_table"), 1.second)
+
+        result shouldBe a[Left[_, _]]
+        result.left.get shouldBe a[TableNotFound]
+      }
+    }
   }
 }
--- src/main/scala/com/emarsys/rdb/connector/mssql/MsSqlErrorHandling.scala
+++ src/main/scala/com/emarsys/rdb/connector/mssql/MsSqlErrorHandling.scala
@@ -2,13 +2,23 @@ package com.emarsys.rdb.connector.mssql
 
 import akka.NotUsed
 import akka.stream.scaladsl.Source
-import com.emarsys.rdb.connector.common.models.Errors.{ConnectionError, ConnectorError}
+import com.emarsys.rdb.connector.common.models.Errors._
+import com.microsoft.sqlserver.jdbc.SQLServerException
 
 trait MsSqlErrorHandling {
   self: MsSqlConnector =>
 
+  val MSSQL_STATE_QUERY_CANCELLED     = "HY008"
+  val MSSQL_STATE_SYNTAX_ERROR        = "S0001"
+  val MSSQL_STATE_PERMISSION_DENIED   = "S0005"
+  val MSSQL_STATE_INVALID_OBJECT_NAME = "S0002"
+
   private def errorHandler(): PartialFunction[Throwable, ConnectorError] = {
-    case ex: Exception => ConnectionError(ex)
+    case ex: SQLServerException if ex.getSQLState == MSSQL_STATE_QUERY_CANCELLED     => QueryTimeout(ex.getMessage)
+    case ex: SQLServerException if ex.getSQLState == MSSQL_STATE_SYNTAX_ERROR        => SqlSyntaxError(ex.getMessage)
+    case ex: SQLServerException if ex.getSQLState == MSSQL_STATE_PERMISSION_DENIED   => AccessDeniedError(ex.getMessage)
+    case ex: SQLServerException if ex.getSQLState == MSSQL_STATE_INVALID_OBJECT_NAME => TableNotFound(ex.getMessage)
+    case ex: SQLServerException                                                      => ConnectionError(ex)
   }
 
   protected def eitherErrorHandler[T](): PartialFunction[Throwable, Either[ConnectorError, T]] =
